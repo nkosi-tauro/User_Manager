b6c6ab532a6199b7f3e89df9f0d8339c0fe9111b
nkosi-tauro <nkosilati23@yahoo.com>
2021-09-13 14:24:27 +0200

    added new routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -8,12 +8,14 @@ const routes: Array<RouteRecordRaw> = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/user-edit',
+    name: 'UserEditForm',
+    component: () => import(/* webpackChunkName: "UserEditForm" */ '../views/UserEditForm.vue')
+  },
+  {
+    path: '/user-add',
+    name: 'UserAddForm',
+    component: () => import(/* webpackChunkName: "UserAddForm" */ '../components/UserAddForm.vue')
   }
 ]
 
