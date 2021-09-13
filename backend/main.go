d3dfcb9221654859314e0dbf4335e7a6c15cd07c
nkosi-tauro <nkosilati23@yahoo.com>
2021-09-13 12:26:23 +0200

    added mongo database

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+    "github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/nkosi-tauro/User_Manager/config"
+
+)
 
 func main() {
     app := fiber.New()
+	config.ConnectDB()
+
+	app.Use(cors.New())
+    app.Use(logger.New())
 
     app.Get("/", func(c *fiber.Ctx) error {
         return c.SendString("Hello, World 👋!")
